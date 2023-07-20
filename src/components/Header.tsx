c9825465303c3acdb70abc2e40d308513f729cd8
Alec Pedersen <96690104+alecsorenpedersen@users.noreply.github.com>
2023-07-20 09:59:10 +0100

    chore: style heading

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,5 @@
-// Navbar.js
-
 import styled from 'styled-components';
+import { motion } from 'framer-motion';
 
 const Nav = styled.nav`
 	background: #fff;
@@ -10,15 +9,26 @@ const Nav = styled.nav`
 	width: 100%;
 `;
 
-const Heading = styled.h1`
+const Heading = styled(motion.h1)`
 	color: #333;
 	font-size: 2rem;
 `;
 
 function Navbar() {
+	const headingVariants = {
+		hidden: { opacity: 0, scale: 0.85 },
+		visible: { opacity: 1, scale: 1 },
+	};
+
 	return (
 		<Nav>
-			<Heading>Modern Toilets</Heading>
+			<Heading
+				variants={headingVariants}
+				initial='hidden'
+				animate='visible'
+				transition={{ duration: 0.5 }}>
+				Victorian Modern Toilets 🚽
+			</Heading>
 		</Nav>
 	);
 }
